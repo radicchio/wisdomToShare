5de1c408c316f8876e8b45d683441432e8bfe233
Raddichio <kathrynrichards241@gmail.com>
2018-05-28 21:44:35 +0100

    initial setup

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+var React = require("react");
+
+class myClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>This is it, at last</h1>
+      </div>
+    );
+  }
+}
+
+export default myClass;
